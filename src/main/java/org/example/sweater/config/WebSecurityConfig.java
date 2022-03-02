f39f1835b0ccbc425c061a5ac6efb8141c9cf7c2
Raysog <w.fedorov.ivan@gmail.com>
2022-03-02 19:43:52 +0300

    add user roles

--- src/main/java/org/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/org/example/sweater/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package org.example.sweater.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
@@ -28,8 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public UserDetailsService userDetailsService() {
 		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("u")
-				.password("l")
+				.username("user")
+				.password("pass")
 				.roles("USER")
 				.build();
 		return new InMemoryUserDetailsManager(user);
